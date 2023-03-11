8dc82036d577b5a6de212f6b67eee0df37c9c24a
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-03-11 17:14:26 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import App from "./App";
 import "./index.css";
-import { Details_link, Filter_link, Home_link, Login_link, Pelis_link, Register_link } from "./Links/links";
+import { Filter_link, Home_link, Login_link, Pelis_link, Register_link } from "./Links/links";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -13,7 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path={Register_link} element={<h1>Register</h1>}/>
         <Route path={Login_link} element={<h1>Login</h1>}/>
         <Route path={Pelis_link} element={<h1>Pelis</h1>}/>
-        <Route path={Details_link} element={<h1>Detalles</h1>}/>
         <Route path={Filter_link} element={<h1>Filter</h1>}/>
       </Routes>
     </BrowserRouter>
