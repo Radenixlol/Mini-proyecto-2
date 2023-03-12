692f6a8def4bc2143e5c93011884298495f52b01
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-03-12 18:38:00 -0400

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'Google': "url(https://d2slcw3kip6qmk.cloudfront.net/app/common/3rd-party-logos/google-logo-2018-no-border.svg)",
+        'Netflix_background':'url(https://images.lifestyleasia.com/wp-content/uploads/2019/11/01120945/netflix-image.jpg)',
       },
     },
   },
