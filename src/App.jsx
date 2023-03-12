cfdd8f2788bad0f4cd9116d7b857562e4e01914e
Jose Calcaño <jose.calcano.dev@gmail.com>
2023-03-12 14:47:18 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div>
+        <>
+        <Navbar />
+        </>
   )
 }
 
